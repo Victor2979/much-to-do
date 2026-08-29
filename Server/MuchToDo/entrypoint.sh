869fb4d653a7c929b90c7d8a44f301b885a0228a
Victor2979 <madubuobuvictorakachukwu@gmail.com>
2026-08-29 10:40:06 +0100

    Add CI/CD pipelines, Dockerfile and entrypoint for AWS deployment

---
+++ Server/MuchToDo/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+ENV_FILE="/app/.env"
+cat > $ENV_FILE <<EOF
+PORT=${PORT:-8080}
+MONGO_URI=${MONGO_URI}
+DB_NAME=${DB_NAME:-much_todo_db}
+JWT_SECRET_KEY=${JWT_SECRET_KEY:-change-me}
+JWT_EXPIRATION_HOURS=${JWT_EXPIRATION_HOURS:-72}
+ENABLE_CACHE=${ENABLE_CACHE:-false}
+REDIS_ADDR=${REDIS_ADDR:-}
+REDIS_PASSWORD=${REDIS_PASSWORD:-}
+LOG_LEVEL=${LOG_LEVEL:-INFO}
+LOG_FORMAT=${LOG_FORMAT:-json}
+EOF
+echo "[entrypoint] Starting Much-to-Do API"
+exec /app/app
